f0a034f7223425a15f29a86c97e18697859223cc
Jakson Almeida <jakson.almeida@estudante.ufjf.br>
2026-01-24 07:57:27 -0300

    Trabalho 3

---
+++ T3/bola_viga_controle.m
@@ -0,0 +1,38 @@
+clc; clear; close all
+syms s b3 b2 b1 b0 a0 real
+
+K = (5/7)*9.81;
+Ts=0.02;
+Dp = s^2;
+Np = K;
+
+Nc = b3*s^3 + b2*s^2 + b1*s + b0;
+Dc = (s^2 + 1^2)*(s + a0);
+
+phi_cl = expand(Dp*Dc + Np*Nc);
+
+p1 = -0.7*4 + 1i*4*sqrt(1-0.7^2);
+p2 = conj(p1);
+p3 = -16;
+p4 = -16;
+p5 = -20;
+
+phi_d = expand((s-p1)*(s-p2)*(s-p3)*(s-p4)*(s-p5));
+
+[clc,~] = coeffs(phi_cl,s,'All');
+[dc, ~] = coeffs(phi_d, s,'All');
+
+eqs = clc == dc;
+
+sol = solve(eqs,[b3 b2 b1 b0 a0]);
+
+C = tf(double([sol.b3 sol.b2 sol.b1 sol.b0]), ...
+       conv([1 0 12],[1 double(sol.a0)]))
+
+Cd = c2d(C, Ts, 't');
+
+disp('Controlador discreto Cd(z):')
+Cd
+
+[numCd, denCd] = tfdata(Cd,'v');
+[Ad, Bd, Cd_mat, Dd] = ssdata(Cd);
